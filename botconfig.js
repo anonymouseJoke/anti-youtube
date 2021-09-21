2c194918d9edf3cf2863c3279e8f1c7aca4d2a93
Joke <87341400+anonymouseJoke@users.noreply.github.com>
2021-09-21 23:08:09 +0200

    Add files via upload

---
+++ botconfig.js
@@ -0,0 +1,44 @@
+module.exports = {
+  Admins: ["UserID", "UserID"], //Admins of the bot
+  ExpressServer: true,//If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || "!", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/VVPTGvvnXS", //Support Server Link
+  Token: process.env.Token || "ODY0NTU3NjY4MDI3MDcyNTQy.YO3MEg.AwaL66_ylf_4XmF8A6J0efFgxoA", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "864557668027072542", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "-ljrH1M6rviD-7mzIWnZKYK0LuiXtQec", //Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
+  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: "Pikachu is cute", //A Secret like a password
+  IconURL:
+    "https://cdn.discordapp.com/avatars/884512000355491881/66426dcb593636adcb9562537f4c379d.webp?size=4096", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  Permissions: 2205280576, //Bot Inviting Permissions
+  Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
+
+  //Lavalink
+   Lavalink: {
+    id: "Main",
+    host: "lava.link",
+    port: 80,
+    pass: "youshallnotpass", 
+    secure: false, // Set this to true if you're self-hosting lavalink on replit.
+  },
+  
+  //Alternate Lavalink
+  /*
+  Lavalink: {
+    id: "Main",
+    host: "lava.sudhan.tech",
+    port: 1234,
+    pass: "CodingWithSudhan", 
+    secure: false // Set this to true if you're self-hosting lavalink on replit.
+  },
+  */
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "c5822d5513134f37aff4fb41c604e76c", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "de970fdbe1e84956afed78494c8add04", //Spotify Client Secret
+  },
+};
